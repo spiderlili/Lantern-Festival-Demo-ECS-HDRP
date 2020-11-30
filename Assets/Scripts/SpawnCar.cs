65fc2cf82e1ec27fc440e85bb26a8ada65fe1c27
Jing <jtan1993@gmail.com>
2020-11-30 22:30:41 +0000

    spawn car

---
+++ Assets/Scripts/SpawnCar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCar : MonoBehaviour
+{
+    public GameObject carPf;
+    public GameObject cameraPf;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 pos = new Vector3(10, 10, 10);
+            GameObject carInstance = Instantiate(carPf, pos, Quaternion.identity);
+
+            //get the camera to follow the car
+            cameraPf.GetComponent<SmoothFollow>().target = carInstance.transform;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnCubes.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject cubePf;
+    public int rows = 10;
+    public int columns = 10;
+    public float perlinMultiplier = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = 0; x < rows; x++)
+        {
+            for (int z = 0; z < columns; z++)
+            {
+                GameObject cubePfInstance = Instantiate(cubePf);
+                //generate 2D perlin noise between 0.0 - 1.0
+                Vector3 pos = new Vector3(x, Mathf.PerlinNoise(x * perlinMultiplier, z * perlinMultiplier), z);
+                cubePfInstance.transform.position = pos;
+            }
+        }
+    }
+}
