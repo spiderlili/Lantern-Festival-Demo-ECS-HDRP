990dbd9ea1807df74f1d0c1254134c38cf090541
Jing <jtan1993@gmail.com>
2020-12-05 19:23:34 +0000

    proxy script init

---
+++ Assets/Scripts/PerlinCubePosProxyECS.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Will become the component on the entity for a game object
+public class PerlinCubePosProxyECS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
