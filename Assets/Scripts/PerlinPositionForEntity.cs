90a6efd540d74e9ca993b1be521c5ce56b7cb823
Jing <jtan1993@gmail.com>
2020-12-06 00:04:39 +0000

    entity creator

--- Assets/Scripts/PerlinPositionForEntity.cs
+++ Assets/Scripts/PerlinPositionForEntity.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Unity.Entities;
 
-//light weight structure that holds data & methods. create a component that will stick onto entity
-public struct PerlinPositionForEntity
+//light weight structure that holds data & methods. create a component that will stick onto entity (perlin position system entity structure)
+public struct PerlinPositionForEntity : IComponentData
 {
 
 }
