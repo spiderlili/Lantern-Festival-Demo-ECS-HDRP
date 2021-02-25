74421cf7607f297378b7e137afc44b71b3883ca9
Jing <jtan1993@gmail.com>
2021-02-25 22:28:45 +0000

    bloom

---
+++ Assets/Scripts/Lantern/SpawnerLantern.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+//entities have a list of component data
+public struct SpawnerLantern : IComponentData
+{
+    //prefab to spawn
+    public Entity prefabLantern;
+
+    //how far away from spawner = radius
+    public float maxDistanceFromSpawner;
+
+    //how often to spawn: how many seconds between each spawn
+    public float secondsBetweenSpawns;
+
+    //how long it has been since last spawn
+    public float secondsToNextSpawn;
+}
